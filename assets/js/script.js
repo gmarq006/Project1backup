6e7a93b70e1c1b0c69bc2ea29b6c5a4f030e5b1d
Devin <devinhill2911@gmail.com>
2023-07-31 19:26:02 -0400

    added api keys and code to js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,19 @@ form.addEventListener('submit', handleSubmit);
 
 // WE STILL NEED TO FIND AND ADD OUR API KEYS, AS WELL AS A FUNCTION TO MAKE SURE THAT THE ZIPCODE ISNT BLANK ALSO LINK TO LOCAL STORAGE
 
+var url = 'https://fitness-calculator.p.rapidapi.com/burnedcalorie?activityid=bi_1&activitymin=25&weight=75';
+var options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5b717f999dmshcf3f7738a0d374ep188399jsn58bf32e76f17',
+		'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
+	}
+};
+
+try {
+	var response = await fetch(url, options);
+	var result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
