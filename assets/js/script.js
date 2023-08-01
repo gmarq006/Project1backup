d67add9c21d5cbf95db8d8aa225885df6fb17b54
Devin <devinhill2911@gmail.com>
2023-07-31 20:58:00 -0400

    updated js and api keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,19 @@ var lNameInput = document.getElementById('lname');
 var genderInput = document.getElementById('gender');
 var submitInput = document.getElementById('');
 
+async function handleSubmit(event){
+	event.preventDefault();
 
-// WE STILL NEED TO FIND AND ADD OUR API KEYS, AS WELL AS A FUNCTION TO MAKE SURE THAT THE ZIPCODE ISNT BLANK ALSO LINK TO LOCAL STORAGE
+	var heightFeetInput = document.getElementById('fname').value;
+	var lNameInput = document.getElementById('lname').value;
+	var genderInput = document.getElementById('gender').value;
+
+}
+
+
 
 var url = 'https://exerciseapi3.p.rapidapi.com/exercise/name/push%20up';
-var options = {
+var option1 = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '13c980233cmsh892799e9e812b94p101186jsn0eaeebfc61b9',
@@ -16,27 +24,30 @@ var options = {
 };
 
 try {
-	var response = await fetch(url, options);
-	var result = await response.text();
-	console.log(result);
+	var response1 = await fetch(url1, options1);
+	var result1 = await response1.text();
+	console.log(result1);
 } catch (error) {
 	console.error(error);
 }
 
 var url = 'https://body-mass-index-bmi-calculator.p.rapidapi.com/weight-category?bmi=19.2';
 var options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-		'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '13c980233cmsh892799e9e812b94p101186jsn0eaeebfc61b9',
+        'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
+    }
 };
 
 try {
-	var response = await fetch(url, options);
-	var result = await response.text();
-	console.log(result);
+    var response = await fetch(url, options);
+    var result = await response.text();
+    console.log(result);
 } catch (error) {
-	console.error(error);
+    console.error(error);
 }
 
+var form = document.getElementById('myForm');
+
+form.addEventListener("submit",handleSubmit);
