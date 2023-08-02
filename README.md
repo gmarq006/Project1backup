5d5447209fc9e526460b50c7409f16915d130bad
lourdes-dorvilus <lourdesdorvilus@yahoo.com>
2023-08-01 22:33:23 -0400

    added comments to css,js,index, and readme of the final revisions we must include also created if statments for the bmi calculator see code lines 13-30

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# 🏋️‍♀️ excersise wise: BMI Calculator & Recipe Advisor  🏋️‍♀️
+# 🏋️‍♀️ exercisewise: BMI Calculator & Recipe Advisor  🏋️‍♀️
 
 ## Paragraph 1: What was the assignment or task?
 The task for this group project was to create a BMI calculator with an API for BMI information and health recipes. The BMI calculator should take the user's inputs for height and weight and provide them with their Body Mass Index (BMI). Additionally, the calculator should make API requests to retrieve information about the BMI category and health recipes based on the calculated BMI.
 ## Paragraph 2: What was our motivation?
-Our motivation behind building this project was to develop a user-friendly BMI calculator that goes beyond providing just the BMI value. We wanted to enhance the user experience by offering relevant excersise  information, such as the BMI category (underweight, average weight, overweight, or obese), and providing excersise  planners tailored to the user's BMI to promote a excersise ier lifestyle. By combining these features into a single tool, we aimed to make it easier for users to understand their excersise  status and access suitable planners to improve their well-being.
+Our motivation behind building this project was to develop a user-friendly BMI calculator that goes beyond providing just the BMI value. We wanted to enhance the user experience by offering relevant exercise information, such as the BMI category (underweight, average weight, overweight, or obese), and providing exercise  planners tailored to the user's BMI to promote a exercise lifestyle. By combining these features into a single tool, we aimed to make it easier for users to understand their exercise status and access suitable planners to improve their well-being.
 ## Paragraph 3: What problem does it solve?
 Our BMI calculator with API integration aims to address two primary problems:
 It provides users a straightforward and convenient way to calculate their BMI by inputting their height and weight.
-It offers more than just the BMI value by fetching information about the user's BMI category, which helps them understand whether they are in a excersise y weight range or need to make lifestyle changes.
-The calculator retrieves excersise  planners tailored to the user's BMI category, which empowers users to make excersise ier dietary choices and improve their overall excersise  and well-being.
+It offers more than just the BMI value by fetching information about the user's BMI category, which helps them understand whether they are in a healthy weight range or need to make lifestyle changes.
+The calculator retrieves exercise planners tailored to the user's BMI category, which empowers users to make exercise dietary choices and improve their overall exercise and well-being.
 ## Paragraph 4: What did we learn? Why did we build this project?
 Through building this project, we learned several valuable lessons. Firstly, we enhanced our skills in frontend web development, including HTML, CSS, and JavaScript, to create an interactive and visually appealing user interface. We also gained experience working with APIs and making asynchronous API requests to fetch data from the server.
-The project allowed us to apply our knowledge of BMI calculations, excersise  information retrieval, and API integration in a real-world scenario. Additionally, we improved our collaboration and communication skills as we worked together as a team to divide tasks, share ideas, and ensure the successful completion of the project.
-Overall, we built this project to create a helpful tool that promotes excersise  awareness and encourages excersise ier lifestyle choices for users. We believe our project can positively impact users' lives and contribute to a excersise ier society by offering a comprehensive BMI calculator with additional excersise -related information and planners.
+The project allowed us to apply our knowledge of BMI calculations, exercise information retrieval, and API integration in a real-world scenario. Additionally, we improved our collaboration and communication skills as we worked together as a team to divide tasks, share ideas, and ensure the successful completion of the project.
+Overall, we built this project to create a helpful tool that promotes exercise awareness and encourages exercise lifestyle choices for users. We believe our project can positively impact users' lives and contribute to a exercise society by offering a comprehensive BMI calculator with additional exercise-related information and planners.
 ## Table of Contents
 Paragraph 1: What was the assignment or task? Paragraph 2: What was your motivation? Paragraph 3: What problem does it solve? Paragraph 4 What did you learn? Why did you build this project?
 
@@ -24,7 +24,7 @@ Weather Dash Board
 Dashboard Website Image
 
 ## Link to Live Website
-excersise Wise BMI Calculator & planners created with HTML, CSS and Javascript. Use this Link to Preview Live Website
+exerciseWise BMI Calculator & planners created with HTML, CSS and Javascript. Use this Link to Preview Live Website
 
 Credits:
 Team Work Make the Dream Work! We worked together systematically on zoom totaling a total of 15+ Hours Phil Loy, Instructor 
@@ -44,3 +44,4 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 How to Contribute Lourdes M. Dorvilus
 
 Testing Tests Using VS Code & GIT HUB to host the webpage.
+<!-- Need to add Git Hub for Group  Memebers, Link to Live Website and Screenshot and Gif of Website -->
--- script.js
+++ script.js
@@ -1,81 +1,101 @@
-var bmiResultsel = document.getElementById("bmiResults")
-var myGoalInputel = document.getElementById("myGoalinput")
+var bmiResultsel = document.getElementById("bmiResults");
+var myGoalInputel = document.getElementById("myGoalinput");
 
+document
+  .getElementById("myForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault(); // Prevent form submission
 
-document.getElementById("myForm").addEventListener("submit", async function (event) {
-	event.preventDefault(); // Prevent form submission
+    // Get the values from the input fields
+    const height = document.getElementById("heightFeet").value;
+    const weight = document.getElementById("weight").value;
+    const gender = document.getElementById("gender").value;
 
-	// Get the values from the input fields
-	const height = document.getElementById("heightFeet").value;
-	const weight = document.getElementById("weight").value;
-	const gender = document.getElementById("gender").value;
+    if (height === "" || isNaN(height))
+      result.innerHTML = "Provide a valid height";
+    else if (weight === "" || isNaN(weight))
+      result.innerHTML = "Provide a valid weight";
+    // if both the input is valid we will calculate the BMI value
+    else {
+      let bmi = (weight / (height * height) / 10000).toFixed(2);
 
-	// Display the collected data
-	alert("Height: " + height + " cm\nWeight: " + weight + " kg\nGender: " + gender);
-	var storeBmi = JSON.parse(await bmiRequest(weight, height * 12))
-	console.log(storeBmi)
-	bmiResultsel.innerHTML = `<p> ${storeBmi.bmi} </p>`
-	exerciseRequest()
-	localStorage.setItem("pastBmi", JSON.stringify(storeBmi))
-});
-async function displayGoal (){
-var storemyGoalInput = await goalRequest()
-	console.log(storemyGoalInput)
-	myGoalInputel.innerHTML += `<p> ${storemyGoalInput.goalRequest} </p>`
-	localStorage.setItem("goal", JSON.stringify(storemyGoalInput))
+      // rounding up bmi to the nearest decimal places(2)
+      if (bmi < 18.6) {
+        result.innerHTML = "Under Weight " + bmi;
+      } else if (bmi > 18.6) {
+        result.innerHTML === "Normal Weight " + bmi;
+      }
+
+	  else if (bmi >= 18.6 && bmi <= 24.9)
+	  result.innerHTML = "Over Weight " + bmi
+    
+
+    // we should add an event listener to the buttons
+    // Display the collected data
+    alert(
+      "Height: " + height + " ft\nWeight: " + weight + " kg\nGender: " + gender
+    );
+    var storeBmi = JSON.parse(await bmiRequest(weight, height * 12));
+    console.log(storeBmi);
+    bmiResultsel.innerHTML = `<p> ${storeBmi.bmi} </p>`;
+    exerciseRequest();
+    localStorage.setItem("pastBmi", JSON.stringify(storeBmi));
+  });
+async function displayGoal() {
+  var storemyGoalInput = await goalRequest();
+  console.log(storemyGoalInput);
+  myGoalInputel.innerHTML += `<p> ${storemyGoalInput.goalRequest} </p>`;
+  localStorage.setItem("goal", JSON.stringify(storemyGoalInput));
 }
-
-
 
 var bmiRequest = async function (weight, height) {
-	const url = `https://body-mass-index-bmi-calculator.p.rapidapi.com/imperial?weight=${weight}&height=${height}`;
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': '3c8416fb24msh5c310a41db76d13p16c570jsnc83bfc85258f',
-			'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
-		}
-	};
+  const url = `https://body-mass-index-bmi-calculator.p.rapidapi.com/imperial?weight=${weight}&height=${height}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "3c8416fb24msh5c310a41db76d13p16c570jsnc83bfc85258f",
+      "X-RapidAPI-Host": "body-mass-index-bmi-calculator.p.rapidapi.com",
+    },
+  };
 
-	try {
-		const response = await fetch(url, options);
-		const result = await response.text();
-		console.log(result);
-		return result
-	} catch (error) {
-		console.error(error);
-	}
-
-}
-
-
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 var goalRequest = async function () {
+  const url =
+    "https://workout-planner1.p.rapidapi.com/customized?time=30&equipment=dumbbells&muscle=biceps&fitness_level=beginner&fitness_goals=strength";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "02c1bf1c50msha479b0dca5111dbp133b1djsn8872783a0a2e",
+      "X-RapidAPI-Host": "workout-planner1.p.rapidapi.com",
+    },
+  };
+  // Workout Planner API Key
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+displayGoal();
 
 
-	const url = 'https://workout-planner1.p.rapidapi.com/customized?time=30&equipment=dumbbells&muscle=biceps&fitness_level=beginner&fitness_goals=strength';
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': '3c8416fb24msh5c310a41db76d13p16c570jsnc83bfc85258f',
-			'X-RapidAPI-Host': 'workout-planner1.p.rapidapi.com'
-		}
-	};
-
-	try {
-		const response = await fetch(url, options);
-		const result = await response.json();
-		console.log(result);
-		return result;
-	} catch (error) {
-		console.error(error);
-	}
-
-}
-
-displayGoal()
-
-
-
-
-
+// if ( condition ) { the action
+// } else if ( condition ) {
+// the action 
+// } else {
+//  the act
+// append the buttons and actions with js... we are not supposed to have alerts.
+// if statements and one last function for the exercise planner or include it in that specific function.
