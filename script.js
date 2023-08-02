b0a20555aa4aad9181f521097ef79876aaaa060a
lourdes-dorvilus <lourdesdorvilus@yahoo.com>
2023-08-02 12:28:10 -0400

    updated the bmi formula to refelct height in innches instead of feet and weight to lbs fixed the if statements in the function

--- script.js
+++ script.js
@@ -1,73 +1,88 @@
 var bmiResultsel = document.getElementById("bmiResults");
 var myGoalInputel = document.getElementById("myGoalinput");
-
+// the variables to be called when pulled from user input from HTML.
 document
   .getElementById("myForm")
   .addEventListener("submit", async function (event) {
     event.preventDefault(); // Prevent form submission
 
+
     // Get the values from the input fields
-    const height = document.getElementById("heightFeet").value;
+    const feet = document.getElementById("heightFeet").value;
+
+    const inches = document.getElementById("heightInches").value;
+
+    const height = parseInt(feet) * 12 + parseInt(inches);
+    console.log(height);
+    console.log(inches);
+    console.log(feet);
     const weight = document.getElementById("weight").value;
     const gender = document.getElementById("gender").value;
-
-    if (height === "" || isNaN(height))
-      result.innerHTML = "Provide a valid height";
-    else if (weight === "" || isNaN(weight))
-      result.innerHTML = "Provide a valid weight";
+// if statements within the function the user input is with valid height and weight to calculate the bmi.
+    if (isNaN(height)) {
+      bmiResultsel.innerHTML = "Provide a valid height";
+      alert("Please enter a valid height")
+    } else if (isNaN(weight)) {
+      bmiResultsel.innerHTML = "Provide a valid weight";
+      alert("Please enter a valid weight")
     // if both the input is valid we will calculate the BMI value
-    else {
-      let bmi = (weight / (height * height) / 10000).toFixed(2);
+    } else {
+      
+      let bmi =  weight / (height * height) * 703
 
+      console.log(height)
+      console.log(bmi)
       // rounding up bmi to the nearest decimal places(2)
       if (bmi < 18.6) {
-        result.innerHTML = "Under Weight " + bmi;
+        bmiResultsel.innerHTML = "Under Weight " + bmi;
       } else if (bmi > 18.6) {
-        result.innerHTML === "Normal Weight " + bmi;
+        bmiResultsel.innerHTML === "Normal Weight " + bmi;
+      }else if (bmi >= 18.6 && bmi <= 24.9) {
+      bmiResultsel.innerHTML = "Over Weight " + bmi
       }
-
-	  else if (bmi >= 18.6 && bmi <= 24.9)
-	  result.innerHTML = "Over Weight " + bmi
+    }  
     
 
     // we should add an event listener to the buttons
     // Display the collected data
     alert(
-      "Height: " + height + " ft\nWeight: " + weight + " kg\nGender: " + gender
+      "Height: " + height + " in\nWeight: " + weight + " lbs\nGender: " + gender
     );
     var storeBmi = JSON.parse(await bmiRequest(weight, height * 12));
     console.log(storeBmi);
     bmiResultsel.innerHTML = `<p> ${storeBmi.bmi} </p>`;
     exerciseRequest();
     localStorage.setItem("pastBmi", JSON.stringify(storeBmi));
-  };
-async function displayGoal() {
-  var storemyGoalInput = await goalRequest();
-  console.log(storemyGoalInput);
-  myGoalInputel.innerHTML += `<p> ${storemyGoalInput.goalRequest} </p>`;
-  localStorage.setItem("goal", JSON.stringify(storemyGoalInput));
-}
+  });
 
-var bmiRequest = async function (weight, height) {
-  const url = `https://body-mass-index-bmi-calculator.p.rapidapi.com/imperial?weight=${weight}&height=${height}`;
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "3c8416fb24msh5c310a41db76d13p16c570jsnc83bfc85258f",
-      "X-RapidAPI-Host": "body-mass-index-bmi-calculator.p.rapidapi.com",
-    },
-  };
 
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-    return result;
-  } catch (error) {
-    console.error(error);
+  async function displayGoal() {
+    var storemyGoalInput = await goalRequest();
+    console.log(storemyGoalInput);
+    myGoalInputel.innerHTML += `<p> ${storemyGoalInput.goalRequest} </p>`;
+    localStorage.setItem("goal", JSON.stringify(storemyGoalInput));
   }
-};
+// api request for bmi
+    var bmiRequest = async function (weight, height) {
+    const url = `https://body-mass-index-bmi-calculator.p.rapidapi.com/imperial?weight=${weight}&height=${height}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "3c8416fb24msh5c310a41db76d13p16c570jsnc83bfc85258f",
+        "X-RapidAPI-Host": "body-mass-index-bmi-calculator.p.rapidapi.com",
+      },
+    };
 
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+};
+// api request for workout planner.
 var goalRequest = async function () {
   const url =
     "https://workout-planner1.p.rapidapi.com/customized?time=30&equipment=dumbbells&muscle=biceps&fitness_level=beginner&fitness_goals=strength";
@@ -91,11 +106,3 @@ var goalRequest = async function () {
 
 displayGoal();
 
-
-// if ( condition ) { the action
-// } else if ( condition ) {
-// the action 
-// } else {
-//  the act
-// append the buttons and actions with js... we are not supposed to have alerts.
-  
